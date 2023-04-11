64fee5200edde4b493290cff8fd1a5b48486f012
Danila Goriachkin <dndv279@gmail.com>
2023-04-11 17:17:20 +0300

    First commit

---
+++ BeRich/BeRichApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeRichApp.swift
+//  BeRich
+//
+//  Created by Danila on 11.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct BeRichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
