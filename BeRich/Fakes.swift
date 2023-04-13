6c7df5f42cc86fe0d783756fe87f126cdf35bc98
Ilya Solovyov <ilya.solovyev@physiq.com>
2023-04-13 15:20:19 +0300

    [19] NavBar created

---
+++ BeRich/Fakes.swift
@@ -0,0 +1,16 @@
+//
+//  Fakes.swift
+//  BeRich
+//
+//  Created by Ilya Solovyov on 13.04.2023.
+//
+
+import Foundation
+
+struct Fakes {
+    static let tickersFakes: [Ticker] = [
+        Ticker(title: "APPLE"),
+        Ticker(title: "GOOGLE"),
+        Ticker(title: "YAHOO")
+    ]
+}
---
+++ BeRich/ListScreen.swift
@@ -0,0 +1,37 @@
+//
+//  ListScreen.swift
+//  BeRich
+//
+//  Created by Danila on 11.04.2023.
+//
+
+import SwiftUI
+
+struct Ticker: Identifiable {
+    var id: String = UUID().uuidString
+    var title: String
+}
+
+struct ListScreen: View {
+    
+    @State private var tickers: [Ticker] = Fakes.tickersFakes
+    
+    var body: some View {
+        
+        NavigationStack {
+            List(tickers) { ticker in
+                Text(ticker.title)
+            }
+            .navigationTitle("BeRich")
+            .toolbarBackground(.red, for: .navigationBar)
+            .navigationBarTitleDisplayMode(.inline)
+            
+        }
+    }
+}
+
+struct ListScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        ListScreen()
+    }
+}
