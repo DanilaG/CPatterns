35a965920e47b7201af481270d45d83695624710
Danila Goriachkin <dndv279@gmail.com>
2023-07-10 12:05:45 +0300

    Clean up ccording 223-ФЗ

--- Doji/DetailedTickerScreen/PatternCellView.swift
+++ Doji/DetailedTickerScreen/PatternCellView.swift
@@ -7,40 +7,22 @@ struct PatternCellView: View {
     var body: some View {
         Group {
             HStack {
-                VStack(alignment: .leading) {
-                    HStack {
+                HStack {
+                    VStack(alignment: .leading) {
                         Text(patternViewData.title)
                             .font(.title2)
                             .foregroundColor(patternViewData.isSelected ? Color.blueMain : .black)
                         Spacer()
-                        HStack(spacing: 0) {
-                            Text(patternViewData.formatedProbability)
-                            Image(
-                                systemName: patternViewData.prediction.direction == .growing ?
-                                    "arrow.up.right" : "arrow.down.right"
-                            )
-                        }
-                        .fontWeight(SwiftUI.Font.Weight.semibold)
-                        .foregroundColor(
-                            patternViewData.isSelected ?
-                                Color.blueMain :
-                                (patternViewData.prediction.probability < 0.6 ?
-                                    Color.gray500 :
-                                    (patternViewData.prediction.direction == .growing ? Color.greenMain : Color.redMain)
-                                )
-                        )
-                    }
-                    HStack {
                         Text(patternViewData.dateInterval)
                             .font(Font.subheadline)
                             .foregroundColor(patternViewData.isSelected ? Color.blueMain : .gray500)
-                        Spacer()
-                        Button(action: infoAction, label: {
-                            Image(systemName: "questionmark.circle")
-                                .imageScale(.medium)
-                                .foregroundColor(patternViewData.isSelected ? Color.blueMain : .gray500)
-                        }).buttonStyle(.plain)
                     }
+                    Spacer()
+                    Button(action: infoAction, label: {
+                        Image(systemName: "questionmark.circle")
+                            .imageScale(.large)
+                            .foregroundColor(patternViewData.isSelected ? Color.blueMain : .gray500)
+                    }).buttonStyle(.plain)
                 }
             }
             .padding(.horizontal, 16)
